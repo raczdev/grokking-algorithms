2ddd6799ceb55327b8d1762e6f7dd88a5814d3d2
Pedro Racz <pedrorecz@hotmail.com>
2024-08-24 05:12:20 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import searches.BinarySearch;
+
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Binary Search
+         */
+        int [] myList = {-1, -2, 3, 4, 5};
+        int myNumber = -1;
+        BinarySearch.findNumber(myList, myNumber);
+        /*
+         * Binary Search
+         */
+
+        
+    }
+}
---
+++ src/searches/BinarySearch.java
@@ -0,0 +1,29 @@
+package searches;
+
+public class BinarySearch {
+    
+    public static void findNumber(int[] arrayList, int numberToFind) {
+        int left = 0;
+        int right = arrayList.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if(arrayList[mid] == numberToFind) {
+                System.out.println("Found it! Position: " + mid + " Number: " + arrayList[mid]);
+                return;
+            } else if (arrayList[mid] > numberToFind) {
+                System.out.println("Not found on Position: " + mid);
+                right = mid - 1;
+                continue;
+            } else if (arrayList[mid] < numberToFind) {
+                System.out.println("Not found on Position: " + mid);
+                left = mid + 1;
+                continue;
+            }
+        }
+
+        System.out.println("Number not found in the list that was given");
+    }
+
+}
